181c122a2b90663ae5f6a1205bf1eb8ca72feb71
jfelz27 <285329710+jFelz27@users.noreply.github.com>
2026-06-06 13:56:47 -0500

    Set base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/beacon_safewalkhome/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
